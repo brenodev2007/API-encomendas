e83b0d39834e37ed437be2453293464687e7eeef
Breno Soriani <breno.soriani@gmail.com>
2025-07-07 18:17:17 -0300

    Cadastrando Entrega

--- src/controllers/deliveriesController.ts
+++ src/controllers/deliveriesController.ts
@@ -1,9 +1,25 @@
 import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+import { prisma } from "@/database/prisma";
 
 export class deliveriesController {
   create = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.json({ message: "ok" });
+      const bodySchema = z.object({
+        user_id: z.string().uuid(),
+        description: z.string(),
+      });
+
+      const { user_id, description } = bodySchema.parse(req.body);
+
+      await prisma.delivery.create({
+        data: {
+          userId: user_id,
+          description: description,
+        },
+      });
+
+      res.json({ message: "Created" });
     } catch (error) {
       next(error);
     }
