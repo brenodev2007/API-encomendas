e35160e5a5a50f32c0b777946d10d853bd2647d7
Breno Soriani <breno.soriani@gmail.com>
2025-07-07 18:07:33 -0300

    Verificando Autorização

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 
 import { deliveriesController } from "@/controllers/deliveriesController";
 import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 
 export const deliverieRoutes = Router();
 
 const deliverieController = new deliveriesController();
 
-deliverieRoutes.use(ensureAuthenticated);
+deliverieRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]));
 
 deliverieRoutes.post("/", deliverieController.create);
