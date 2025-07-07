07ce2764a6a555b6d84c46e582919dd64ef981b7
Breno Soriani <breno.soriani@gmail.com>
2025-07-07 15:59:25 -0300

    Cadastrando usuário no banco de dados

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,18 +1,31 @@
 import { Request, Response, NextFunction } from "express";
+import { prisma } from "@/database/prisma";
 import { hash } from "bcrypt";
 import { z } from "zod";
 
 export class userController {
   create = async (req: Request, res: Response, next: NextFunction) => {
-    const bodySchema = z.object({
-      name: z.string().nonempty(),
-      email: z.string().email(),
-      password: z.string().min(6),
-    });
+    try {
+      const bodySchema = z.object({
+        name: z.string().nonempty(),
+        email: z.string().email(),
+        password: z.string().min(6),
+      });
 
-    const { name, email, password } = bodySchema.parse(req.body);
-    const hashedPassword = await hash(password, 8);
+      const { name, email, password } = bodySchema.parse(req.body);
+      const hashedPassword = await hash(password, 8);
 
-    res.json({ message: "User created successfully", hashedPassword });
+      const user = prisma.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      });
+
+      res.json({ message: "User created successfully", user });
+    } catch (error) {
+      next(error);
+    }
   };
 }
