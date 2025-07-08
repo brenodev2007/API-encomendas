97cb695676bc3082b807dd734b7a1bbc36db4e40
Breno Soriani <breno.soriani@gmail.com>
2025-07-08 15:25:44 -0300

    Configurando a porta para deploy

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { app } from "@/app";
 import dotenv from "dotenv";
 import "dotenv/config";
+import { env } from "./utils/env";
 dotenv.config();
 
-const PORT = 3333;
+const PORT = env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
