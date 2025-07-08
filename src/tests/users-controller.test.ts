a266a64efc0c92a82f69f28d7c5cdf5adcc1a9fd
Breno Soriani <breno.soriani@gmail.com>
2025-07-08 15:01:22 -0300

    Teste no Cadastro do Usuário

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,3 +1,17 @@
+import request from "supertest";
+
+import { app } from "@/app";
+
 describe("userController", () => {
-  console.log("Passou por aqui ");
+  it("should create a new user succesfully", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Test User",
+      email: "testuser@example.com",
+      password: "senha12345",
+    });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.name).toBe("Test User");
+  });
 });
