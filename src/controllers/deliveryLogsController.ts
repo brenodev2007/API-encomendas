f7202298b403a4bff7fe76dd0ebd5e32ae110ac9
Breno Soriani <breno.soriani@gmail.com>
2025-07-08 14:21:18 -0300

    Cadastrando Log

--- src/controllers/deliveryLogsController.ts
+++ src/controllers/deliveryLogsController.ts
@@ -1,7 +1,37 @@
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
 import { Request, Response, NextFunction } from "express";
+import z from "zod";
 
 export class deliveryLogsController {
   create = async (req: Request, res: Response, next: NextFunction) => {
-    res.json({ message: "ok" });
+    const bodySchema = z.object({
+      delivery_id: z.string().uuid(),
+      description: z.string(),
+    });
+
+    const { delivery_id, description } = bodySchema.parse(req.body);
+
+    const delivery = await prisma.delivery.findUnique({
+      where: {
+        id: delivery_id,
+      },
+    });
+
+    if (!delivery) {
+      throw new AppError("delivery not found", 404);
+    }
+
+    if (delivery.status === "processing") {
+      throw new AppError("change status to shipped ");
+    }
+
+    await prisma.deliveryLog.create({
+      data: {
+        deliveryId: delivery_id,
+        description,
+      },
+    });
+    res.json();
   };
 }
